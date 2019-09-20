2c3bf9644f68e6867d8d0aeac04d01cdcdca1c68
dajeong.yoon <djyoon956@gmail.com>
2019-09-20 20:21:58 +0900

    테스트중

---
+++ Example/src/InstanceofTest.java
@@ -0,0 +1,21 @@
+import java.sql.Time;
+import java.util.Timer;
+
+class User {
+
+}
+
+class B extends User {
+
+}
+
+class J extends User {
+
+}
+
+public class InstanceofTest {
+	public static void main(String[] args) {
+		
+		
+	}
+}
---
+++ Example/src/Timeeer.java
@@ -0,0 +1,32 @@
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Timeeer {
+	public static void main(String[] args) {
+//		Timer t = new Timer();
+//		TimerTask tt = new TimerTask() {
+//			int time = 0;
+//
+//			@Override
+//			public void run() {
+//				System.out.println(++time + "오형남 멍청이");
+//			};
+//		};
+//		t.schedule(tt, 0, 2000);
+//
+//		try {
+//			t.wait();
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		for (int i = 0; i < 400; i++) {
+//			System.out.println("타이머 밖");
+//		}
+//		System.out.println("타이머 밖");
+
+		String teString = "950506-2222222";
+		Calendar.YEAR-
+	}
+}
