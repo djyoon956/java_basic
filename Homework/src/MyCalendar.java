ac2c731a4e15799cc52c570c19651065d00863cf
dajeong.yoon <djyoon956@gmail.com>
2019-09-27 16:20:46 +0900

    add

---
+++ Homework/src/MyCalendar.java
@@ -0,0 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MyCalendar {
+	public static String date() {
+		return new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
+	}
+
+	public static String time() {
+		return new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
+	}
+}
---
+++ Homework/src/bank/MyCalendar.java
@@ -0,0 +1,13 @@
+package bank;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MyCalendar {
+	public static String date() {
+		return new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
+	}
+
+	public static String time() {
+		return new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
+	}
+}
